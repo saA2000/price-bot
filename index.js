31b850ca1a5f6b8e1c4a4aa3f19e921f87ce9494
saA2000 <116278290+saA2000@users.noreply.github.com>
2022-10-28 23:09:58 +0300

    help ?

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const http = require('http')
-const Web3 = require('web3')
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-const moment = require('moment-timezone')
-const numeral = require('numeral')
-const _ = require('lodash')
-const axios = require('axios')
+import express from 'express'
+import bodyParser from 'body-parser'
+import { createServer } from 'http'
+import Web3 from 'web3'
+import HDWalletProvider from '@truffle/hdwallet-provider'
+import moment from 'moment-timezone'
+import numeral from 'numeral'
+import _ from 'lodash'
+import axios from 'axios'
 
-// SERVER CONFIG
+// SERVER CONFIGget
 const PORT = process.env.PORT || 5000
 const app = express();
-const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const server = createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // WEB3 CONFIG
 const web3 = new Web3(process.env.RPC_URL)
